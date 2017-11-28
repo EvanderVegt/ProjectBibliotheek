8fbc076cf0573a2028f3dcaf32e833245986bdda
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-28 17:23:48 +0100

    laatste boek ingevoerd in bibliotheek weergeven

--- index.php
+++ index.php
@@ -22,24 +22,35 @@
  
 </head>
 
-<body>
+<body text="white">
          
     <?php
             
-
+    
  
     $conn = connectionDB();    // Call for a PHP function // We can not find it on this page SO it must be on the shared page generalfunctions.php
 
-    //-----Notities verwijderen-------------------------------------------------
+
+
+
+
+
+
+
+ 
     
-    if (isset($_POST['delete']) && isset($_POST['Boek_id'])) {
-        $notitie_id = get_post($conn, 'Boek_id');
-        $query = "DELETE FROM `notitie` WHERE `notitie`.`Notitie_id`='$notitie_id'";
-        $result = $conn->query($query);
-        if (!$result)
-            echo "DELETE failed: $query<br>" .
-            $conn->error . "<br><br>";
-    }
+    
+    
+    //-----Notities verwijderen-------------------------------------------------
+//    
+//    if (isset($_POST['delete']) && isset($_POST['Boek_id'])) {
+//        $notitie_id = get_post($conn, 'Boek_id');
+//        $query = "DELETE FROM `notitie` WHERE `notitie`.`Notitie_id`='$notitie_id'";
+//        $result = $conn->query($query);
+//        if (!$result)
+//            echo "DELETE failed: $query<br>" .
+//            $conn->error . "<br><br>";
+//    }
     //-----Notities toevoegen---------------------------------------------------
     
     if (isset($_POST['Notitie'])) {
@@ -53,7 +64,9 @@
             echo "INSERT failed: $query<br>" .
             $conn->error . "<br><br>";
     }
-
+    ?>
+<div id="input notitie">
+    <?php
     //-----Notities (Titel selecteren-Text invoeren en verzenden----------------
     //-----MySql invoerveld "Titel" instellen op VARCHAR 45---------------------
     echo "<form id=\"invoerText\" action=\"index.php\" method=\"post\"><pre>";
@@ -64,32 +77,54 @@
     . '<textarea type="text" name="Notitie" cols="50" rows="3" style="width: 320px; height: 50px;" required>Bladzijde :                                 Notitie   :</textarea>   ';
     echo '<br>                                                                                                                                                               <input type="submit" value="Verzenden">';
     echo '</pre></form>'; 
-  
+ ?>
+</div>
+<?php
+$sql = "SELECT * FROM `boek` ORDER BY `Invoerdatum` DESC LIMIT 1";
 
-    $query = "SELECT * FROM `notitie`";
-    $result = $conn->query($query);
-    if (!$result)
-        die("Database access failed: " . $conn->error);
-
-    $rows = $result->num_rows;
-
-    for ($j = 0; $j < $rows; ++$j) {
-        $result->data_seek($j);
-        $row = $result->fetch_array(MYSQLI_NUM);
-//-------Notities weergeven op index.php----------------------------------------
-// echo <<<_END
-//<pre>
-//    Notitie_id: $row[0]
-//    Notitie   : $row[1]
-//
-//</pre>
-//    <form action="index.php" method="post">
-//    <input type="hidden" name="delete" value="yes">
-//    <input type="hidden" name="Boek_id" value="$row[0]">
-//    <input type="submit" value="DELETE RECORD"></form>
-//_END;
- 
+$result = $conn->query($sql);
+?>
+    <div id="laatst">
+        <?php
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> Op ". $row["Invoerdatum"]. " is als laatst een boek (". $row['Titel'].") " .
+                "<br> van de schrijfer ". $row["Auteur"]. " aan de bibliotheek toegevoegd.<br>";
     }
+} else {
+    echo "0 results";
+}
+?>
+    </div>
+    <?php
+//    
+//        $query = "SELECT * FROM `boek`";
+//    $result = $conn->query($query);
+//    if (!$result)
+//        die("Database access failed: " . $conn->error);
+//
+//    $rows = $result->num_rows;
+//
+//    for ($j = 0; $j < $rows; ++$j) {
+//        $result->data_seek($j);
+//        $row = $result->fetch_array(MYSQLI_NUM);
+//
+//        echo <<<_END
+//  <pre>
+//           Boek_id $row[0]
+//       Invoerdatum $row[1]
+//             Titel $row[2]
+//            Auteur $row[3]
+//              Isbn $row[4]
+//          Uitgever $row[5]
+//         Categorie $row[6]
+//           Ranking $row[7]
+//  </pre>
+//
+//_END;
+//    }
+
     $result->close();
     $conn->close();
 
--- notities.php
+++ notities.php
@@ -3,48 +3,51 @@
 
 <html>
     <head>
-        <header>Notities</header>
-        <link rel = "stylesheet" type = "text/css" href="bibliotheek.css">
-        <meta charset="UTF-8">
-        <title>Notities</title>
-           <nav>
-            <div class="topnav" id="myTopnav">
-                <a href="index.php" class="active">Home</a>
-                <a href="toevoegen.php"> Toevoegen </a>
-                <a href="bibliotheek.php"> Bibliotheek </a>
-                <a href="notities.php">Notities </a>
-            </div>
-            </nav>
-    </head>
-    <body>
-        <?php
-        $conn = connectionDB();
-        
-                $query = "SELECT * FROM `boek`";
-    $result2 = $conn->query($query);
-    if (!$result2)
-        die("Database access failed: " . $conn->error);
+    <header>Notities</header>
+    <link rel = "stylesheet" type = "text/css" href="bibliotheek.css">
+    <meta charset="UTF-8">
+    <title>Notities</title>
+    <nav>
+        <div class="topnav" id="myTopnav">
+            <a href="index.php" class="active">Home</a>
+            <a href="toevoegen.php"> Toevoegen </a>
+            <a href="bibliotheek.php"> Bibliotheek </a>
+            <a href="notities.php">Notities </a>
+        </div>
+    </nav>
+</head>
+<body>
+    <?php
+    $conn = connectionDB();
 
-    $rows2 = $result2->num_rows;
+    //------Haalt de data van de Titel uit tafel boek------------------------------
 
-    for ($h = 0; $h < $rows2; ++$h) {
-        $result2->data_seek($h);
-        $row2 = $result2->fetch_array(MYSQLI_NUM);
-    
-   //---------------------------------------------------------------------------     
-            $query = "SELECT * FROM `notitie`";
+    $query = "SELECT * FROM `boek`";
     $result = $conn->query($query);
     if (!$result)
         die("Database access failed: " . $conn->error);
 
     $rows = $result->num_rows;
 
-    for ($j = 0; $j < $rows; ++$j) {
-        $result->data_seek($j);
+    for ($h = 0; $h < $rows; ++$h) {
+        $result->data_seek($h);
         $row = $result->fetch_array(MYSQLI_NUM);
-         echo <<<_END
+
+        //------Haalt de data van Notitie uit tafel notitie----------------------------   
+
+        $query = "SELECT * FROM `notitie`";
+        $result = $conn->query($query);
+        if (!$result)
+            die("Database access failed: " . $conn->error);
+
+        $rows = $result->num_rows;
+
+        for ($j = 0; $j < $rows; ++$j) {
+            $result->data_seek($j);
+            $row = $result->fetch_array(MYSQLI_NUM);
+            echo <<<_END
 <pre>
-    Titel     : $row2[2]    
+    Titel     : $row[2]    
     Notitie_id: $row[0]
     Notitie   : $row[1]
 
@@ -55,15 +58,9 @@
     <input type="hidden" name="Boek_id" value="$row[0]">
     <input type="submit" value="DELETE RECORD"></form>
 _END;
- 
-   
+        }
     }
-        function get_post($conn, $var) {
-        return $conn->real_escape_string($_POST[$var]);
-    }
-    
-    }
-?>
-    </body>   
+    ?>
+</body>   
 </html>
 
